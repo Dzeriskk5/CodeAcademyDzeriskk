c9e8b85193fbbf4ce7021fa91d8fc62d655c0ca3
Dzeriskk5 <dzeriskk@gmail.com>
2024-12-18 20:30:57 +0200

    asdasd

--- 47TerraformYay/backend.tf
+++ 47TerraformYay/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "your-backend-bucket-name"
+    bucket         = "Backend-Bucket"
     key            = "terraform/state/${var.environment}"
     region         = "eu-central-1"
     encrypt        = true
